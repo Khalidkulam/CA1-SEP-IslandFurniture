5f3cff4d5adedc1c05b476cd67989dd0f033705e
Thaika968 <thaikajaffer@gmail.com>
2026-01-01 19:06:50 +0800

    Completed furniture rename and added Retail top-level menu

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "1234",
+            password: "Thaika",
             database: "islandfurniture-it07"
         });
         return conn;
